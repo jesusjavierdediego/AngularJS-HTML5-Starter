35fc11d5b77d9daa37c58ed098b7cc3d04a7d952
jdediego <jdediego@fexco.com>
2015-03-14 13:02:18 +0000

    Created show form and implemented controls based on angular only

--- app/scripts/controllers/multiform-controllers.js
+++ app/scripts/controllers/multiform-controllers.js
@@ -18,64 +18,101 @@ angular.module('App.Controllers')
   $scope.oneAtATime = true;
 
   $scope.groups = RESTFactory.readList('sideMenu');
-  
+
   $scope.setMenuItem = function(itemId){
       $rootScope.menuItem = itemId;
   };
-  
-  //$log.debug("MultiformController/$location.url(): " + $location.path().split("/")[2]||"Unknown");
+
   $scope.status = {
     isFirstOpen: true,
     isFirstDisabled: false
   };
 }])
 
-.controller('PerformManualEntryCtrl', ["$log", "$scope",  
-    function ($log, $scope) {
-        $log.debug("Loaded PerformManualEntryCtrl");
-        
-        $('.selectpicker').selectpicker({
-            style: 'btn-info',
-            size: 4
-        });
-}])
+.controller('PerformManualEntryCtrl', ["$log", "$scope", "RESTFactory",
+        function($log, $scope, RESTFactory) {
 
-.controller('DatepickerDemoCtrl', ["$scope", "$log",
-    function ($scope, $log) {
-        $log.debug("Loaded DatepickerDemoCtrl");
-        
-        $scope.today = function() {
-          $scope.dt = new Date();
-        };
-        $scope.today();
+            //Initialize status of controls
+            $scope.debitAgentStatus = true;
 
-        $scope.clear = function () {
-          $scope.dt = null;
-        };
+            //Data sources
+            $scope.debitLedgerTypes = RESTFactory.readList('debitLedgerTypes');
+            $scope.creditLedgerTypes = RESTFactory.readList('creditLedgerTypes');
+            $scope.debitAgents = RESTFactory.readList('debitAgents');
+            $scope.creditAgents = RESTFactory.readList('creditAgents');
+            $scope.txTypes = RESTFactory.readList('txTypes');
 
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-          return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-        };
 
-        $scope.toggleMin = function() {
-          $scope.minDate = $scope.minDate ? null : new Date();
-        };
-        $scope.toggleMin();
+            //capture events from UI controls and implement behavior
+            $scope.changeDebitAgentStatus = function(){
+                $log.debug("change in control.");
+                if($scope.so1Object.debitLedgerType){
+                    $scope.debitAgentStatus = false;
+                }else{
+                    $scope.debitAgentStatus = true;
+                }
 
-        $scope.open = function($event) {
-          $event.preventDefault();
-          $event.stopPropagation();
+            };
 
-          $scope.opened = true;
-        };
+            //Data model
+            $scope.so1Object = {};
+            $scope.so1Object.debitLedgerType = {};
+            $scope.so1Object.debitLedgerType.id;
+            $scope.so1Object.debitLedgerType.name;
+            $scope.so1Object.creditLedgerType = {};
+            $scope.so1Object.creditLedgerType.id;
+            $scope.so1Object.creditLedgerType.name;
+            $scope.so1Object.debitAgent = {};
+            $scope.so1Object.debitAgent.id;
+            $scope.so1Object.debitAgent.name;
+            $scope.so1Object.creditAgent = {};
+            $scope.so1Object.creditAgent.id;
+            $scope.so1Object.creditAgent.name;
+            $scope.so1Object.txType = {};
+            $scope.so1Object.txType.id;
+            $scope.so1Object.txType.name;
+            $scope.so1Object.amount;
+            $scope.so1Object.comment;
+            $scope.so1Object.relatedMTCN;
+            $scope.so1Object.MTCNDate;
+            $scope.so1Object.entryDate;
+            $scope.so1Object.excludedEntryFromDailyStatement;
 
-        $scope.dateOptions = {
-          formatYear: 'yy',
-          startingDay: 1
-        };
 
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
+            $scope.sendForm = function(){
+                $log.debug("Object to send: " + angular.toJson($scope.so1Object, true));
+            };
+            //Example: You can validate data in controller as well, eg against server
+           /* $scope.sendForm = {
+                onSubmit: function(event) {
+                    $log.debug("Object to send: " + angular.toJson($scope.so1Object, true));
+                    if (dataIsntValid) {
+                        displayErrors();
+                        event.preventDefault();
+                    }else {
+                        submitData();
+                    }
+                }
+            };*/
+
+            $scope.resetForm = function(){
+                $scope.so1Object = {};
+
+
+                $log.debug("Form has been reset.");
+            };
+
+/*            myApp.factory('myRestService', ['Restangular', function(Restangular) {
+                return {
+                    myFunction: function(varA, varB, varC) {
+                        var path = "rest/primaryPath/" + varA + "/" + varB + "/" + varC;
+                        var restObject = Restangular.all(path);
+                        var thePromise = restObject.getList();
+
+                        return thePromise;
+                    }
+                };
+            }]);*/
+
 }]);
 
