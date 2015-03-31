bd6a9bf0a7756764888cd6d3cb267b270ff312e7
jdediego <jdediego@fexco.com>
2015-03-31 14:01:16 +0100

    Fixed one hide menu error

--- app/scripts/controllers/multiform-controllers.js
+++ app/scripts/controllers/multiform-controllers.js
@@ -7,55 +7,55 @@
 angular.module('App.Controllers')
 
 
-.controller('MultiformController', ['$log', '$scope', '$state', '$stateParams', 'utils', 'RESTFactory',
-        function ($log, $scope, $state, $stateParams, utils, RESTFactory) {
+        .controller('MultiformController', ['$log', '$scope', '$state', '$stateParams', 'utils', 'RESTFactory',
+            function ($log, $scope, $state, $stateParams, utils, RESTFactory) {
 
-    }])
+            }])
 
 
-.controller('SideMenuCtrl', ["$log", "$rootScope", "$scope", "$stateParams", "$location", "RESTFactory",
-    function ($log, $rootScope, $scope, $stateParams, $location, RESTFactory) {
-  $scope.oneAtATime = false;
-  $scope.toggle = true;
-  $scope.toggleSideMenuButtonValue = "<";
-  $scope.toggleSideMenu = function(){
-    $scope.toggleAction = !$scope.toggleAction;
-    $scope.toggle = !$scope.toggle;
-    if($scope.toggleSideMenuButtonValue == "<")
-        $scope.toggleSideMenuButtonValue = ">";
-    else
-        $scope.toggleSideMenuButtonValue = "<";
-  };
+        .controller('SideMenuCtrl', ["$log", "$rootScope", "$scope", "$stateParams", "$location", "RESTFactory",
+            function ($log, $rootScope, $scope, $stateParams, $location, RESTFactory) {
+                $scope.oneAtATime = false;
+                $scope.toggle = true;
+                $scope.toggleSideMenuButtonValue = "<";
+                $scope.toggleSideMenu = function () {
+                    $scope.toggleAction = !$scope.toggleAction;
+                    $scope.toggle = !$scope.toggle;
+                    if ($scope.toggleSideMenuButtonValue == "<")
+                        $scope.toggleSideMenuButtonValue = ">";
+                    else
+                        $scope.toggleSideMenuButtonValue = "<";
+                };
 
-  $scope.groups = RESTFactory.readList('sideMenu');
+                $scope.groups = RESTFactory.readList('sideMenu');
 
-  $scope.setMenuItem = function(itemId){
-      $rootScope.menuItem = itemId;
-  };
+                $scope.setMenuItem = function (itemId) {
+                    $rootScope.menuItem = itemId;
+                };
 
-  $scope.status = {
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-}])
+                $scope.status = {
+                    isFirstOpen: true,
+                    isFirstDisabled: false
+                };
+            }])
 
-.controller('PerformManualEntryCtrl', ["$log", "$rootScope", "$scope", "$q", "RESTFactory",
-        function($log, $rootScope, $scope, $q, RESTFactory) {
+        .controller('PerformManualEntryCtrl', ["$log", "$scope", "$q", "RESTFactory",
+            function ($log, $scope, $q, RESTFactory) {
 
-            //Initialize status of controls
-            $scope.debitAgentStatus = true;
-            $scope.creditLedgerTypes = '';
-            $scope.debitAgents = '';
-            $scope.creditAgents = '';
-            $scope.txTypes = '';
+                //Initialize status of controls
+                $scope.debitAgentStatus = true;
+                $scope.creditLedgerTypes = '';
+                $scope.debitAgents = '';
+                $scope.creditAgents = '';
+                $scope.txTypes = '';
 
-            //Data sources
-            $scope.debitLedgerTypes = RESTFactory.readList('debitLedgerTypes');
-            $scope.creditLedgerTypes = RESTFactory.readList('creditLedgerTypes');
-            $scope.debitAgents = RESTFactory.readList('debitAgents');
-            $scope.creditAgents = RESTFactory.readList('creditAgents');
-            $scope.txTypes = RESTFactory.readList('txTypes');
-            
+                //Data sources
+                $scope.debitLedgerTypes = RESTFactory.readList('debitLedgerTypes');
+                $scope.creditLedgerTypes = RESTFactory.readList('creditLedgerTypes');
+                $scope.debitAgents = RESTFactory.readList('debitAgents');
+                $scope.creditAgents = RESTFactory.readList('creditAgents');
+                $scope.txTypes = RESTFactory.readList('txTypes');
+
 //            var promises = [];
 //            var paths = [];
 //            paths.push('debitLedgerTypes');
@@ -72,105 +72,105 @@ angular.module('App.Controllers')
 //                promises.push(RESTFactory.readBatch(path));
 //            });
 //            $q.all(promises).then(showView = true);
-            $rootScope.resolved = false;
-            $log.debug("resolved? " + $rootScope.resolved);
-            $scope.$watch(
-                    function(){
-                         if(
-                            $scope.debitLedgerTypes != '' &&
-                            $scope.creditLedgerTypes != '' &&
-                            $scope.debitAgents != '' &&
-                            $scope.creditAgents != '' &&
-                            $scope.txTypes != ''
-                        ){
-                            $rootScope.resolved = true
-                            $log.debug("resolved? " + $rootScope.resolved);
+                $scope.resolved = false;
+                $scope.loading = true;
+                $scope.$watch(
+                        function () {
+                            if (
+                                    $scope.debitLedgerTypes != '' &&
+                                    $scope.creditLedgerTypes != '' &&
+                                    $scope.debitAgents != '' &&
+                                    $scope.creditAgents != '' &&
+                                    $scope.txTypes != ''
+                                    ) {
+                                $scope.loading = false;
+                                $scope.resolved = true;
+                            }
                         }
+
+                );
+
+                //capture events from UI controls and implement behavior
+                $scope.changeDebitAgentStatus = function () {
+                    $log.debug("change in control: " + $scope.so1Object.debitLedgerType);
+                    if ($scope.so1Object.debitLedgerType) {
+                        $scope.debitAgentStatus = false;
+                    } else {
+                        $scope.debitAgentStatus = true;
                     }
-                    
-            );
-           
-            //capture events from UI controls and implement behavior
-            $scope.changeDebitAgentStatus = function(){
-                $log.debug("change in control: " + $scope.so1Object.debitLedgerType);
-                if($scope.so1Object.debitLedgerType){
-                    $scope.debitAgentStatus = false;
-                }else{
-                    $scope.debitAgentStatus = true;
-                }
 
-            };
+                };
 
-            //Data model
-            $scope.so1Object = {};
-            $scope.so1Object.debitLedgerType = {};
-            $scope.so1Object.debitLedgerType.id;
-            $scope.so1Object.debitLedgerType.name;
-            $scope.so1Object.creditLedgerType = {};
-            $scope.so1Object.creditLedgerType.id;
-            $scope.so1Object.creditLedgerType.name;
-            $scope.so1Object.debitAgent = {};
-            $scope.so1Object.debitAgent.id;
-            $scope.so1Object.debitAgent.name;
-            $scope.so1Object.creditAgent = {};
-            $scope.so1Object.creditAgent.id;
-            $scope.so1Object.creditAgent.name;
-            $scope.so1Object.txType = {};
-            $scope.so1Object.txType.id;
-            $scope.so1Object.txType.name;
-            $scope.so1Object.amount;
-            $scope.so1Object.comment;
-            $scope.so1Object.relatedMTCN;
-            $scope.so1Object.MTCNDate;
-            $scope.so1Object.entryDate;
-            $scope.so1Object.excludedEntryFromDailyStatement;
+                //Data model
+                $scope.so1Object = {};
+                $scope.so1Object.debitLedgerType = {};
+                $scope.so1Object.debitLedgerType.id;
+                $scope.so1Object.debitLedgerType.name;
+                $scope.so1Object.creditLedgerType = {};
+                $scope.so1Object.creditLedgerType.id;
+                $scope.so1Object.creditLedgerType.name;
+                $scope.so1Object.debitAgent = {};
+                $scope.so1Object.debitAgent.id;
+                $scope.so1Object.debitAgent.name;
+                $scope.so1Object.creditAgent = {};
+                $scope.so1Object.creditAgent.id;
+                $scope.so1Object.creditAgent.name;
+                $scope.so1Object.txType = {};
+                $scope.so1Object.txType.id;
+                $scope.so1Object.txType.name;
+                $scope.so1Object.amount;
+                $scope.so1Object.comment;
+                $scope.so1Object.relatedMTCN;
+                $scope.so1Object.MTCNDate;
+                $scope.so1Object.entryDate;
+                $scope.so1Object.excludedEntryFromDailyStatement;
 
 
-            //You can validate data in controller as well, eg against server
-            /*$scope.sendForm = function(so1Object) {
-                $scope.PerformManualEntryForm.submitted = true;
-                if($scope.PerformManualEntryForm.$valid) {
-                    $log.debug("Object to send: " + angular.toJson(so1Object, true));
-                } else {
-                    $log.debug('Errors in form data');
-                }
-            };*/
+                //You can validate data in controller as well, eg against server
+                /*$scope.sendForm = function(so1Object) {
+                 $scope.PerformManualEntryForm.submitted = true;
+                 if($scope.PerformManualEntryForm.$valid) {
+                 $log.debug("Object to send: " + angular.toJson(so1Object, true));
+                 } else {
+                 $log.debug('Errors in form data');
+                 }
+                 };*/
 
-            $scope.sendForm = function(so1Object) {
+                $scope.sendForm = function (so1Object) {
 //                if($scope.so1Object.amount.slice(-1) == "."){
 //                    $scope.so1Object.amount = $scope.so1Object.amount.slice(0,-1);
 //                }
-                $scope.PerformManualEntryForm.submitted = true;
-                $log.debug("Object to send: " + angular.toJson(so1Object, true));
-            };
+                    $scope.PerformManualEntryForm.submitted = true;
+                    $log.debug("Object to send: " + angular.toJson(so1Object, true));
+                };
 
-            $scope.resetForm = function(){
-                $scope.so1Object = {};
-                $log.debug("Form has been reset.");
-            };
+                $scope.resetForm = function () {
+                    $scope.so1Object = {};
+                    $log.debug("Form has been reset.");
+                };
 
-}])
+            }])
 
-.controller('TypeaheadDemoCtrl', ['$scope', '$templateCache', '$http',
-    function($scope, $templateCache, $http) {
+        .controller('TypeaheadDemoCtrl', ['$scope', '$templateCache', '$http',
+            function ($scope, $templateCache, $http) {
 
-  $scope.selectedState = '';
-  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+                $scope.selectedState = '';
+                $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
-  $scope.selectedIcon = '';
+                $scope.selectedIcon = '';
 
-  $scope.selectedAddress = '';
-  $scope.getAddress = function(viewValue) {
-    var params = {address: viewValue, sensor: false};
-    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
-    .then(function(res) {
-      return res.data.results;
-    });
-  };
-  
-  $scope.selectedItem = '';
-  $scope.items = ['FIRST', 'SECOND'];
+                $scope.selectedAddress = '';
+                $scope.getAddress = function (viewValue) {
+                    var params = {address: viewValue, sensor: false};
+                    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+                            .then(function (res) {
+                                return res.data.results;
+                            });
+                };
 
-}]);
+                $scope.selectedItem = '';
+                $scope.items = ['FIRST', 'SECOND'];
+
+            }]);
 
 
