e37f72d0df9895450adc4c81acad2a1e76f7d467
jdediego <jdediego@fexco.com>
2015-04-01 09:35:02 +0100

    Modification on the controllers tests in unit and midway phases

--- test/midway/controllers/controllersSpec.js
+++ test/midway/controllers/controllersSpec.js
@@ -12,7 +12,7 @@ describe("Midway: Testing Controllers", function () {
         });
     });
 
-    it('should load the /home route by default', function (done) {
+    it('should load the /multiform route by default', function (done) {
         tester.visit('/', function () {
             expect(tester.path()).to.equal('/multiform');
             var current;
@@ -25,5 +25,18 @@ describe("Midway: Testing Controllers", function () {
             });
         });
     });
+//    it('should load the view ao1.html when /ao1 route is accessed', function (done) {
+//        tester.visit('/ao1', function () {
+//            expect(tester.path()).to.equal('/ao1');
+//            var current;
+//            tester.until(function () {
+//                current = tester.inject('$state').current;
+//                return current.templateUrl;
+//            }, function () {
+//                expect(current.templateUrl).to.equal('views/multiform/partials/ao1.html');
+//                done();
+//            });
+//        });
+//    });
 
 });
--- test/unit/controllers/controllersSpec.js
+++ test/unit/controllers/controllersSpec.js
@@ -17,10 +17,26 @@ describe('Unit: Testing Controllers', function () {
         controller('PerformManualEntryCtrl', {
             '$scope': scope,
             'RESTFactory': {
-                readList: function () {}
+                readList: function () {},
+                readBatch: function () {}
             }
+                 
         });
-        expect(scope.notes).to.equal(undefined);
+        //flags
+        expect(scope.loading).to.be.a.boolean;
+        expect(scope.resolved).to.be.a.boolean;
+        //retrieved data for the form
+        expect(scope.debitLedgerTypes).to.be.an.array;
+        expect(scope.creditLedgerTypes).to.be.an.array;
+        expect(scope.debitAgents).to.be.an.array;
+        expect(scope.creditAgents).to.be.an.array;
+        expect(scope.txTypes).to.be.an.array;
+        //model
+        expect(scope.so1Object).to.be.an.array;
+        //methods
+        expect(scope.changeDebitAgentStatus).to.be.a.function;
+        expect(scope.sendForm).to.be.a.function;
+        expect(scope.resetForm).to.be.a.function;
     });
 
 
