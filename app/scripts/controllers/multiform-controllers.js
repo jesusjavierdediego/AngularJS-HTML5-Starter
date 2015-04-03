228e45d56562ad8e4e58927ee9528eb17d4cda35
jdediego <jdediego@fexco.com>
2015-04-03 11:20:41 +0100

    deleted undefined var in validation

--- app/scripts/controllers/multiform-controllers.js
+++ app/scripts/controllers/multiform-controllers.js
@@ -118,7 +118,6 @@ angular.module('App.Controllers')
                  };*/
 
                 $scope.sendForm = function (so1Object) {
-                    $scope.PerformManualEntryForm.submitted = true;
                     $log.debug("Object to send: " + angular.toJson(so1Object, true));
                 };
 
